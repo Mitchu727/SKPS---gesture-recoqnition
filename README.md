179ce13eaca33a27bfc4f528504e405aca5a3a5e
Rokarolka <romanowskarolina@gmail.com>
2021-04-26 20:20:27 +0200

    browser view- server and client

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ pip install -r requirements.txt
 ```
 then use the command and go to the indicated address
 ```
-uvicorn main:app
+uvicorn main:app --host 0.0.0.0 --port 8000
 ```
+Then go to your browser and type in __localhost:8000__.  
+That's all. Have a nice use!
---
+++ main.py
@@ -0,0 +1,22 @@
+# host computer
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+html = ""
+with open('html/index.html', 'r') as f:
+    html = f.read()
+
+@app.get("/")
+async def get(request: Request):
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    color = "#7FFFD4"
+    while color != "q":
+        color = input("Podaj kolor: ")
+        await websocket.send_text(color)
+    await websocket.close()
