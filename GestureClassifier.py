4a4a20409cb09f27aa16b1deda5975588fe3cf10
Rokarolka <romanowskarolina@gmail.com>
2021-05-21 23:16:57 +0200

    get stats

--- GestureClassifier.py
+++ GestureClassifier.py
@@ -3,4 +3,27 @@ class GestureClassifer:
         self.classify_quan = classify_quan
 
     def classify(self, last_rois):
-        print(last_rois[-self.classify_quan:])
+        sample = last_rois[-self.classify_quan:]
+
+        min_hight = min(sample, key=lambda x: x[1])
+        max_hight = max(sample, key=lambda x: x[1])
+
+        min_hight_index = sample.index(min_hight)
+        max_hight_index = sample.index(max_hight)
+
+        diff_hight_index = max_hight_index - min_hight_index
+        diff_hight = max_hight[1] - min_hight[1]
+
+        min_width = min(sample, key=lambda x: x[0])
+        max_width = max(sample, key=lambda x: x[0])
+        
+        min_width_index = sample.index(min_width)
+        max_width_index = sample.index(max_width)
+
+        diff_width_index = max_width_index - min_width_index
+        diff_width = max_width[0] - min_width[0]
+
+        self.choose_gesture(diff_hight_index, diff_hight, diff_width_index, diff_width)
+
+    def choose_gesture(self, diff_hight_index, diff_hight, diff_width_index, diff_width):
+        print(diff_hight_index, diff_hight, diff_width_index, diff_width)
